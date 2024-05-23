bf3188a6bd3ac82abf6c0bc98309a0f1ca6ef223
Yasunori Ishiguro <xjr1300.04@gmail.com>
2024-05-23 13:55:11 +0900

    会員区分(MembershipType)に一般会員と特別会員を取得するクラスメソッドを実装

--- drugstore/domain/models/membership_types.py
+++ drugstore/domain/models/membership_types.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from enum import Enum
-from typing import List, Tuple
+from typing import List, Optional, Self, Tuple
 
 
 class MembershipTypeCode(Enum):
@@ -17,6 +17,21 @@ MEMBERSHIP_TYPE_DATA: List[Tuple[int, str]] = [
     (MembershipTypeCode.General.value, "一般会員"),
     (MembershipTypeCode.Special.value, "特別会員"),
 ]
+
+
+def find_membership_data(code: int) -> Optional[Tuple[int, str]]:
+    """会員区分データを返す。
+
+    Args:
+        code (int): 会員区分コード
+
+    Returns:
+        Tuple[int, str]: 会員区分コードと会員区分名を格納したタプル
+    """
+    for pair in MEMBERSHIP_TYPE_DATA:
+        if pair[0] == code:
+            return pair
+    return None
 
 
 @dataclass
@@ -43,3 +58,29 @@ class MembershipType:
             raise ValueError("会員区分コードと会員区分名の組み合わせが不正です。")
         self.code = data[0]
         self.name = data[1]
+
+    @classmethod
+    def general_membership_type(cls) -> Self:
+        """一般会員を取得
+
+        Returns:
+            Self: 一般会員
+        """
+        data = find_membership_data(MembershipTypeCode.General.value)
+        if data:
+            return cls(data[0], data[1])
+        else:
+            raise NotImplementedError
+
+    @classmethod
+    def special_membership_type(cls) -> Self:
+        """特別会員を取得
+
+        Returns:
+            Self: 特別会員
+        """
+        data = find_membership_data(MembershipTypeCode.Special.value)
+        if data:
+            return cls(data[0], data[1])
+        else:
+            raise NotImplementedError
