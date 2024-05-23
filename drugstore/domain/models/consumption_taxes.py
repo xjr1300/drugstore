ab8692e21445100ff2b0b02163050be4fa562e23
Yasunori Ishiguro <xjr1300.04@gmail.com>
2024-05-23 16:32:36 +0900

    消費税率(ConsumptionTax)のイニシャライザを実装

--- drugstore/domain/models/consumption_taxes.py
+++ drugstore/domain/models/consumption_taxes.py
@@ -23,3 +23,38 @@ class ConsumptionTax:
     end: datetime
     # 消費税率
     rate: Decimal
+
+    def __init__(
+        self, id: uuid.UUID, begin: datetime, end: datetime, rate: Decimal
+    ) -> None:
+        """イニシャライザ
+
+        Args:
+            id (uuid.UUID): 消費税ID
+            begin (datetime): 消費税の起点日時
+            end (datetime): 消費税の終点日時
+            rate (Decimal): 消費税の税率
+
+        Raises:
+            ValueError: 消費税の起点日時と終点日時は日本標準時でなくてはなりません。
+            ValueError: 消費税の起点日時が範囲外です。
+            ValueError: 消費税の終点日時が範囲外です。
+            ValueError: 消費税の起点日時が終点日時以降です。
+            ValueError: 消費税の税率が0.0未満または1.0以上です。
+        """
+        if begin.tzinfo != JST or end.tzinfo != JST:
+            raise ValueError(
+                "消費税の起点日時と終点日時は日本標準時でなくてはなりません。"
+            )
+        if begin < MIN_CONSUMPTION_TAX_BEGIN or MAX_CONSUMPTION_TAX_END <= begin:
+            raise ValueError("消費税の起点日時が範囲外です。")
+        if end <= MIN_CONSUMPTION_TAX_BEGIN or MAX_CONSUMPTION_TAX_END < end:
+            raise ValueError("消費税の終点日時が範囲外です。")
+        if end <= begin:
+            raise ValueError("消費税の起点日時が終点日時以降です。")
+        if rate < Decimal("0.0") or Decimal("1.0") <= rate:
+            raise ValueError("消費税の税率が0.0未満または1.0以上です。")
+        self.id = id
+        self.begin = begin
+        self.end = end
+        self.rate = rate
