894455a7694b33170fc368ab9625ce59aa713f92
Yasunori Ishiguro <xjr1300.04@gmail.com>
2024-05-24 13:32:54 +0900

    消費税の期間が重複なくまた途切れなく連続していることを確認する関数の単体テストを実装

--- tests/drugstore/utils/test_consumption_tax_manager.py
+++ tests/drugstore/utils/test_consumption_tax_manager.py
@@ -1,16 +1,92 @@
 import unittest
+import uuid
+from datetime import datetime, timedelta
+from decimal import Decimal
+
+from drugstore.common import JST
+from drugstore.domain.models.consumption_taxes import ConsumptionTax
+from drugstore.utils.consumption_tax_manager import (
+    ensure_consumption_tax_periods_are_continuous,
+)
+
+# 連続した消費税のリスト
+CONTINUOUS_CONSUMPTION_TAXES = [
+    ConsumptionTax(
+        uuid.uuid4(),
+        datetime(2024, 1, 1, tzinfo=JST),
+        datetime(2024, 4, 1, tzinfo=JST),
+        Decimal("0.05"),
+    ),
+    ConsumptionTax(
+        uuid.uuid4(),
+        datetime(2024, 4, 1, tzinfo=JST),
+        datetime(2024, 7, 1, tzinfo=JST),
+        Decimal("0.10"),
+    ),
+    ConsumptionTax(
+        uuid.uuid4(),
+        datetime(2024, 7, 1, tzinfo=JST),
+        datetime(2024, 9, 1, tzinfo=JST),
+        Decimal("0.15"),
+    ),
+    ConsumptionTax(
+        uuid.uuid4(),
+        datetime(2024, 9, 1, tzinfo=JST),
+        datetime(2025, 1, 1, tzinfo=JST),
+        Decimal("0.20"),
+    ),
+]
 
 
 class ConsumptionTaxesTest(unittest.TestCase):
-    """消費税リストテスト
+    """消費税リストテスト"""
 
-    # TODO: 次を確認する単体テストを実装すること
+    def test_ensure_consumption_taxes_are_continuous(self) -> None:
+        """消費税の期間が重複または途切れなく連続している場合に、期間が連続していると判定することを確認"""
+        result = ensure_consumption_tax_periods_are_continuous(
+            CONTINUOUS_CONSUMPTION_TAXES
+        )
 
-    - 消費税を複数格納したリストについて、消費税の期間が重複または途切れなく連続している場合にTrueを返す
-    - 消費税を1つ格納したリストの場合にTrueを返す
-    - 消費税を複数格納したリストについて、消費税の期間に重複がある場合にFalseを返す
-    - 消費税を複数格納したリストについて、消費税の期間に途切れがある場合にFalseを返す
-    """  # noqa: E501
+        self.assertTrue(result)
+
+    def test_ensure_one_consumption_taxes_is_continuous(self) -> None:
+        """1つの消費税を期間が連続していると判定することを確認"""
+        taxes = [
+            ConsumptionTax(
+                uuid.uuid4(),
+                datetime(2024, 1, 1, tzinfo=JST),
+                datetime(2025, 1, 1, tzinfo=JST),
+                Decimal("0.10"),
+            ),
+        ]
+
+        result = ensure_consumption_tax_periods_are_continuous(taxes)
+
+        self.assertTrue(result)
+
+    def test_ensure_consumption_taxes_with_interrupted_periods_are_not_continuous(
+        self,
+    ) -> None:
+        """期間が途切れている消費税のリストを、期間が連続していないと判定することを確認"""
+        taxes = CONTINUOUS_CONSUMPTION_TAXES.copy()
+        # 2番目の消費税を削除して、期間を途切れさせる
+        del taxes[1]
+
+        result = ensure_consumption_tax_periods_are_continuous(taxes)
+
+        self.assertFalse(result)
+
+    def test_ensure_consumption_taxes_with_overlapped_periods_are_not_continuous(
+        self,
+    ) -> None:
+        """期間が重複している消費税のリストを、期間が連続していないと判定することを確認"""
+        taxes = CONTINUOUS_CONSUMPTION_TAXES.copy()
+        # 最初と次の消費税の期間を重複させる
+        taxes[1].begin = taxes[1].begin - timedelta(seconds=1)
+
+        result = ensure_consumption_tax_periods_are_continuous(taxes)
+
+        self.assertFalse(result)
 
 
 class ConsumptionTaxManagerTest(unittest.TestCase):
