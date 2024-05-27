6174ab7004c9540e96e10679c172a87a613596d1
kuroyasu <xjr1300.04@gmail.com>
2024-05-27 17:44:32 +0900

    売上のイニシャライザの単体テストをリスト

--- drugstore/domain/models/sales.py
+++ drugstore/domain/models/sales.py
@@ -66,3 +66,23 @@ class Sale:
     consumption_tax_amount: Decimal
     # 合計額
     total: Decimal
+
+    def __init__(
+        self,
+        customer: Optional[Customer],
+        sold_at: datetime,
+        consumption_tax_rate: Decimal,
+    ) -> None:
+        """イニシャライザ
+
+        Args:
+            customer (Optional[Customer]): 顧客
+            sold_at (datetime): 売上日時
+            consumption_tax_rate (Decimal): 消費税率
+
+        TODO: 次の単体テストを実装
+
+        - 妥当な引数でインスタンスを構築できることを確認
+        - 売上日時が日本標準時以外の場合に、例外をスローすることを確認
+        - 消費税額がドメインで定められた範囲外の場合に、例外をスローすることを確認
+        """
