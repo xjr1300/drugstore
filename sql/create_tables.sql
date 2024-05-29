1e4405d5809448669b26f8fd351c268548726890
kuroyasu <xjr1300.04@gmail.com>
2024-05-29 16:42:36 +0900

    Implement sql statements for drug store database.

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -3,15 +3,15 @@ CREATE TABLE IF NOT EXISTS items (
     id TEXT NOT NULL,
     name TEXT NOT NULL,
     unit_price INTEGER NOT NULL,
-    PRIMARY KEY (id),
-    CHECK (unit_price >= 0)
+    CONSTRAINT pk_items PRIMARY KEY (id),
+    CONSTRAINT ck_items_unit_price CHECK (unit_price >= 0)
 );
 
 -- 会員区分テーブル
 CREATE TABLE IF NOT EXISTS membership_types (
     code INTEGER NOT NULL,
     name TEXT NOT NULL,
-    PRIMARY KEY (code)
+    CONSTRAINT pk_membership_types PRIMARY KEY (code)
 );
 
 -- 顧客テーブル
@@ -19,49 +19,52 @@ CREATE TABLE IF NOT EXISTS customers (
     id TEXT NOT NULL,
     name TEXT NOT NULL,
     membership_type_code INTEGER NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (membership_type_code) REFERENCES membership_types (code) ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT pk_customers PRIMARY KEY (id),
+    CONSTRAINT fk_customers__membership_type_code FOREIGN KEY (membership_type_code) REFERENCES membership_types (code) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- 消費税テーブル
+-- 消費税率は10,000倍して記録
 CREATE TABLE IF NOT EXISTS consumption_taxes (
     id TEXT NOT NULL,
     begin_dt TEXT NOT NULL,
     end_dt TEXT NOT NULL,
-    rate REAL NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE (begin_dt),
-    UNIQUE (end_dt),
-    CHECK (
-        rate >= 0.0
-        AND rate < 1.0
+    rate INTEGER NOT NULL,
+    CONSTRAINT pk_consumption_taxes PRIMARY KEY (id),
+    CONSTRAINT uq_consumption_taxes_begin_dt UNIQUE (begin_dt),
+    CONSTRAINT uq_consumption_taxes_end_dt UNIQUE (end_dt),
+    CONSTRAINt ck_consumption_taxes_rate CHECK (
+        rate >= 0
+        AND rate < 10000
     )
 );
 
 -- 売上テーブル
+-- 割引率は10,000倍して記録
+-- 消費税率は10,000倍して記録
 CREATE TABLE if NOT EXISTS sales (
     id TEXT NOT NULL,
     customer_id TEXT NULL,
     sold_at TEXT NOT NULL,
     subtotal INTEGER NOT NULL,
-    discount_rate REAL NOT NULL,
+    discount_rate INTEGER NOT NULL,
     discount_amount INTEGER NOT NULL,
     taxable_amount INTEGER NOT NULL,
-    consumption_tax_rate REAL NOT NULL,
+    consumption_tax_rate INTEGER NOT NULL,
     consumption_tax_amount INTEGER NOT NULL,
     total INTEGER NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (customer_id) REFERENCES customers (id) ON DELETE CASCADE ON UPDATE CASCADE,
-    CHECK (subtotal >= 0),
-    CHECK (discount_rate >= 0.0),
-    CHECK (discount_amount >= 0),
-    CHECK (taxable_amount >= 0),
-    CHECK (
-        consumption_tax_rate >= 0.0
-        AND consumption_tax_rate < 1.0
+    CONSTRAINT pk_sales PRIMARY KEY (id),
+    CONSTRAINT fk_sales__customers FOREIGN KEY (customer_id) REFERENCES customers (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT ck_sales_subtotal CHECK (subtotal >= 0),
+    CONSTRAINT ck_sales_discount_rate CHECK (discount_rate >= 0),
+    CONSTRAINT ck_sales_discount_amount CHECK (discount_amount >= 0),
+    CONSTRAINT ck_sales_taxable_amount CHECK (taxable_amount >= 0),
+    CONSTRAINT ck_sales_consumption_tax_rate CHECK (
+        consumption_tax_rate >= 0
+        AND consumption_tax_rate < 10000
     ),
-    CHECK (consumption_tax_amount >= 0),
-    CHECK (total >= 0)
+    CONSTRAINT ck_sales_consumption_tax_amount CHECK (consumption_tax_amount >= 0),
+    CONSTRAINT ck_sales_total CHECK (total >= 0)
 );
 
 -- 売上明細テーブル
@@ -70,9 +73,9 @@ CREATE TABLE IF NOT EXISTS sale_details (
     item_id TEXT NOT NULL,
     quantities INTEGER NOT NULL,
     amount INTEGER NOT NULL,
-    PRIMARY KEY (sale_id, item_id),
-    FOREIGN KEY (sale_id) REFERENCES sales (id) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (item_id) REFERENCES items (id) ON DELETE CASCADE ON UPDATE CASCADE,
-    UNIQUE (sale_id, item_id),
-    CHECK (quantities >= 0) CHECK (amount > 0)
+    CONSTRAINT pk_sale_details PRIMARY KEY (sale_id, item_id),
+    CONSTRAINT fk_sale_details__sales FOREIGN KEY (sale_id) REFERENCES sales (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT fk_sale_details__items FOREIGN KEY (item_id) REFERENCES items (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT uq_sale_details_sale_id_item_id UNIQUE (sale_id, item_id),
+    CONSTRAINT ck_sale_details_quantities CHECK (quantities >= 0) CONSTRAINT ck_sale_details_amount CHECK (amount > 0)
 );
